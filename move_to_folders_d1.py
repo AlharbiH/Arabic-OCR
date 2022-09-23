35fa648d629059fbac7db0eebc2517876f4dfcaf
AlharbiH <100615011+AlharbiH@users.noreply.github.com>
2022-09-23 15:47:07 +0100

    Add files via upload

---
+++ move_to_folders_d1.py
@@ -0,0 +1,10 @@
+import os 
+import shutil
+
+folder = 'train'
+
+for f in os.listdir(folder):
+    f_name = str(f)
+    for i in range(28):
+        if 'label_'+str(i)+'.' in f_name:
+            shutil.move('train/'+f_name, str(i)+'/'+f_name)
